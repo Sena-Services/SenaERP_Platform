bf818af0819ac06c515d5f24d7de0c8ac3bf90fb
Sheltonsena <sheltonjoseph@sena.services>
2026-02-20 10:14:43 +0000

    contact added to the doctype

---
+++ senaerp_platform/senaerp_platform/doctype/waitlist_contract/waitlist_contract.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Sena Technologies and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class WaitlistContract(Document):
+	# This is a child table doctype - no special logic needed
+	pass
